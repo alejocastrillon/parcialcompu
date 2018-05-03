9a5193ae24c3bd52c364820aa273e958c8263f5c
Alejandro <jairocastrillon@misena.edu.co>
2018-05-02 21:05:06 -0500

    Upload source files

--- parcial2.py
+++ parcial2.py
@@ -1,10 +1,3 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-__author__ = "alejandro"
-__date__ = "$2/05/2018 08:18:46 PM$"
-
 import pygame
 
 size = width, height = [680, 400]
