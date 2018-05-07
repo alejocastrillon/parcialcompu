7277ecdc14c46b1bbee2ac761078a9cd94c00ea1
Alejandro <jairocastrillon@misena.edu.co>
2018-05-06 22:07:12 -0500

    Right and down moves implementation

---
+++ source/objetos.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="objetos" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../../../Imágenes/tiledrpg.png" trans="ff00ff" width="512" height="512"/>
+</tileset>
