78a6fe4ea2d34f5e2f8aacc973597a349c387ba8
loperasebastian <sebastian.lopera@utp.edu.co>
2018-05-08 14:02:43 -0500

    se pone a correr la imagen recortada

--- parcial2.py
+++ parcial2.py
@@ -14,31 +14,44 @@ class Jugador(pygame.sprite.Sprite):
 		self.f = matrix
 		self.image = self.f[0][0]
 		self.rect = self.image.get_rect()
-		self.direction = 0
+		self.direction = -1
 		self.index = 0
 
 	def update(self):
 		if self.direction == 1:
-			self.image = self.f[2][self.index]
+			self.image = self.f[1][self.index]
 			self.index += 1
 			if self.index >= 8:
 				self.index = 4
 			if self.rect.x <= width - 150:
 				self.rect.x += 5
 		elif self.direction == 2:
-			self.image = self.f[0][self.index]
+			self.image = self.f[3][self.index]
 			self.index += 1
 			if self.index >= 8:
 				self.index = 4
-			if self.rect.y <= height - 150:
-				self.rect.y += 5
+			if self.rect.x >= 33:
+				self.rect.x += -5
 		elif self.direction == 3:
-			self.image = self.f[4][self.index]
+			self.image = self.f[2][self.index]
 			self.index += 1
 			if self.index >= 8:
 				self.index = 4
 			if self.rect.y >= 5:
 				self.rect.y -= 5
+		elif self.direction == 4:
+			self.image = self.f[0][self.index]
+			self.index += 1
+			if self.index >= 8:
+				self.index = 4
+			if self.rect.y >= 0:
+				self.rect.y += 5
+		elif self.direction == 0:
+			self.image = self.f[4][self.index]
+			self.index +=1
+			if self.index >=9:
+				self.index = 0
+
 
 def generateAmbient():
 	pantalla.blit(imageFondo, [posx, posy])
@@ -50,12 +63,13 @@ def recortarSprite(nombrearchivo, cantidadX, cantidadY):
 	imageInfo = imageSprite.get_rect()
 	imageWidth = imageInfo[2]
 	imageHeight = imageInfo[3]
-	corteX = (imageWidth / cantidadX)-1
+	corteX = 33
 	matrix = []
-	corteY = (imageHeight / cantidadY)-1
+	corteY = (imageHeight / cantidadY)
+	listaCorte = [8,8,8,8,9,9,6,6]
 	for y in range(cantidadY):
 		matrix.append([])
-		for x in range(cantidadX):
+		for x in range(listaCorte[y]):
 			cuadro = imageSprite.subsurface(corteX * x, corteY * y, corteX,corteY)
 			matrix[y].append(cuadro)
 	return matrix
@@ -67,24 +81,28 @@ if __name__ == "__main__":
     imageFondoWidth = imagefondoInfo[2]
     imageFondoHeight = imagefondoInfo[3]
     generateAmbient()
-    matrixKano = recortarSprite('source/mariomoves.png', 8, 5)
+    matrixKano = recortarSprite('source/terminado.png', 8, 8)
     jugador = Jugador(matrixKano)
     jugadores.add(jugador)
     pygame.display.flip()
     done = False
     while not done:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            elif event.type == pygame.KEYDOWN:
-            	if event.key == pygame.K_RIGHT:
-            		jugador.direction = 1
-            	elif event.key == pygame.K_DOWN:
-            		jugador.direction = 2
-            	elif event.key == pygame.K_UP:
-            		jugador.direction = 3
-            	elif event.key == pygame.K_SPACE:
-            		jugador.direction = 0
+			if event.type == pygame.QUIT:
+				done = True
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RIGHT:
+					jugador.direction = 1
+				elif event.key == pygame.K_LEFT:
+					jugador.direction = 2
+				elif event.key == pygame.K_UP:
+					jugador.direction = 3
+				elif event.key == pygame.K_DOWN:
+					jugador.direction = 4
+				elif event.key == pygame.K_SPACE:
+					jugador.direction = 0
+					jugador.index = 0
+
         if jugador.direction == 1 and jugador.rect.x >= width -150 and posx >= width - imageFondoWidth:
         	posx -= 5
         elif jugador.direction == 2 and jugador.rect.y >= height - 150 and posy >= height- imageFondoHeight:
