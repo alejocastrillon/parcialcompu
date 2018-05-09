2e2ff80618191bfd9e7cb2c02d40dc24463d021e
loperasebastian <sebastian.lopera@utp.edu.co>
2018-05-09 14:09:35 -0500

    modificacion hecha en el q

--- parcial2.py
+++ parcial2.py
@@ -14,43 +14,79 @@ class Jugador(pygame.sprite.Sprite):
 		self.f = matrix
 		self.image = self.f[0][0]
 		self.rect = self.image.get_rect()
-		self.direction = -1
+		self.direction = 0
+		self.action = 0
 		self.index = 0
 
+		'''Acciones:
+		1. Puno
+		2. Caminar
+		3. Martillo '''
+
 	def update(self):
-		if self.direction == 1:
+		if self.direction == 1 and self.action == 2:
 			self.image = self.f[1][self.index]
 			self.index += 1
 			if self.index >= 8:
 				self.index = 4
 			if self.rect.x <= width - 150:
 				self.rect.x += 5
-		elif self.direction == 2:
+		elif self.direction == 2 and self.action == 2:
 			self.image = self.f[3][self.index]
 			self.index += 1
 			if self.index >= 8:
 				self.index = 4
 			if self.rect.x >= 33:
 				self.rect.x += -5
-		elif self.direction == 3:
+		elif self.direction == 3 and self.action == 2:
 			self.image = self.f[2][self.index]
 			self.index += 1
 			if self.index >= 8:
 				self.index = 4
-			if self.rect.y >= 5:
+			if self.rect.y >= 10:
 				self.rect.y -= 5
-		elif self.direction == 4:
+		elif self.direction == 4 and self.action == 2:
 			self.image = self.f[0][self.index]
 			self.index += 1
 			if self.index >= 8:
 				self.index = 4
-			if self.rect.y >= 0:
+			if self.rect.y <= height - 150:
 				self.rect.y += 5
-		elif self.direction == 0:
-			self.image = self.f[4][self.index]
+
+		if self.action == 1:
+			if self.direction == 2:
+				self.image = self.f[5][self.index]
+			else:
+				self.image = self.f[4][self.index]
 			self.index +=1
-			if self.index >=9:
+			if self.index >=8:
 				self.index = 0
+				if self.direction == 2:
+					self.image = self.f[3][self.index]
+				else:
+					self.image = self.f[self.direction][self.index]
+				self.action = 0
+		elif self.action == 3:
+			if self.direction == 2:
+				self.image = self.f[7][self.index]
+			else:
+				self.image = self.f[6][self.index]
+			self.index +=1
+			if self.index >=5:
+				self.index = 0
+				if self.direction == 2:
+					self.image = self.f[3][self.index]
+				else:
+					self.image = self.f[self.direction][self.index]
+				self.action = 0
+		'''elif self.action == 1 and self.direction == 2:
+			self.image = self.f[5][self.index]
+			self.index +=1
+			if self.index >=8:
+				self.index = 0
+				self.image = self.f[3][self.index]
+				self.action = 0
+				self.direction = 0'''
 
 
 def generateAmbient():
@@ -91,17 +127,25 @@ if __name__ == "__main__":
 			if event.type == pygame.QUIT:
 				done = True
 			elif event.type == pygame.KEYDOWN:
+				jugador.index = 0
 				if event.key == pygame.K_RIGHT:
 					jugador.direction = 1
+					jugador.action = 2
 				elif event.key == pygame.K_LEFT:
 					jugador.direction = 2
+					jugador.action = 2
 				elif event.key == pygame.K_UP:
 					jugador.direction = 3
+					jugador.action = 2
 				elif event.key == pygame.K_DOWN:
 					jugador.direction = 4
+					jugador.action = 2
 				elif event.key == pygame.K_SPACE:
 					jugador.direction = 0
-					jugador.index = 0
+				elif event.key == pygame.K_p:
+					jugador.action = 1
+				elif event.key == pygame.K_m:
+					jugador.action = 3
 
         if jugador.direction == 1 and jugador.rect.x >= width -150 and posx >= width - imageFondoWidth:
         	posx -= 5
@@ -109,6 +153,8 @@ if __name__ == "__main__":
         	posy -= 5
         elif jugador.direction == 2 and jugador.rect.x>= 20 and posx <= -10:
         	posx += 5
+        elif jugador.direction == 3 and jugador.rect.y >= 40 and posy <= -10:
+        	posy += 5
         generateAmbient()
     	jugadores.draw(pantalla)
     	jugadores.update()
